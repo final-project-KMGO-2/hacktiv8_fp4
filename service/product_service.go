410e2b8768e032c3226731ed314a34f99aa32811
Khoirul Anam <khairulanamstud@gmail.com>
2022-12-07 08:54:29 +0700

    working on product services

--- service/product_service.go
+++ service/product_service.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"hacktiv8_fp_2/entity"
 	"hacktiv8_fp_2/repository"
+
+	"github.com/mashingan/smapping"
 )
 
 type ProductService interface {
-	CreateNewProduct(ctx context.Context, productCrt entity.ProductCreate)
-	GetAllProducts(ctx context.Context)
-	UpdateProductById(ctx context.Context, productUpd entity.ProductUpdate, id uint64)
-	DeleteProductById(ctx context.Context, id uint64)
+	CreateNewProduct(ctx context.Context, productCrt entity.ProductCreate) (entity.Product, error)
+	GetAllProducts(ctx context.Context) ([]entity.Product, error)
+	UpdateProductById(ctx context.Context, productUpd entity.ProductUpdate, id uint64) (entity.Product, error)
+	DeleteProductById(ctx context.Context, id uint64) error
 }
 
 type productService struct {
@@ -21,8 +23,49 @@ func NewProductService(pr repository.ProductRepository) ProductService {
 	return &productService{productRepo: pr}
 }
 
-func (pr *productService) CreateNewProduct(ctx context.Context, productCrt entity.ProductCreate) {}
-func (pr *productService) GetAllProducts(ctx context.Context) {}
-func (pr *productService) UpdateProductById(ctx context.Context, productUpd entity.ProductUpdate, id uint64){}
-func (pr *productService) DeleteProductById(ctx context.Context, id uint64){}
+// TODO: Check category every create and update
 
+func (pr *productService) CreateNewProduct(ctx context.Context, productCrt entity.ProductCreate) (entity.Product, error) {
+	product := entity.Product{}
+	err := smapping.FillStruct(&product, smapping.MapFields(&productCrt))
+	if err != nil {
+		return entity.Product{}, err
+	}
+
+	// check category di sini
+
+	result, err := pr.productRepo.InsertNewProduct(ctx, product)
+	if err != nil {
+		return entity.Product{}, err
+	}
+	return result, nil
+}
+
+func (pr *productService) GetAllProducts(ctx context.Context) ([]entity.Product, error) {
+	result, err := pr.productRepo.SelectAllProducts(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (pr *productService) UpdateProductById(ctx context.Context, productUpd entity.ProductUpdate, id uint64) (entity.Product, error) {
+	product := entity.Product{}
+	err := smapping.FillStruct(&product, smapping.MapFields(&productUpd))
+	if err != nil {
+		return entity.Product{}, err
+	}
+	result, err := pr.productRepo.UpdateProduct(ctx, product, id)
+	if err != nil {
+		return entity.Product{}, err
+	}
+	return result, nil
+}
+
+func (pr *productService) DeleteProductById(ctx context.Context, id uint64)  error{
+	err := pr.productRepo.DeleteProduct(ctx, id);
+	if err != nil {
+		return err;
+	}
+	return nil
+}
