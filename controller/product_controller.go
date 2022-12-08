67d4033962e5c796f0dcb511f41be2f7b19eb887
Khoirul Anam <khairulanamstud@gmail.com>
2022-12-08 10:07:21 +0700

    working on product controller

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"hacktiv8_fp_2/common"
+	"hacktiv8_fp_2/entity"
 	"hacktiv8_fp_2/service"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +17,7 @@ type ProductController interface {
 	DeleteProductById(ctx *gin.Context)
 }
 
+// TODO: buy new product 
 
 type productController struct {
 	jwtService service.JWTService
@@ -23,7 +28,79 @@ func NewProductController(jt service.JWTService, ps service.ProductService) Prod
 	return &productController{jwtService: jt, productService: ps}
 }
 
-func (ps *productController) AddNewProduct(ctx *gin.Context) {}
-func (ps *productController) GetAllProducts(ctx *gin.Context) {}
-func (ps *productController) UpdateProductByID(ctx *gin.Context) {}
-func (ps *productController) DeleteProductById(ctx *gin.Context) {}
+func (ps *productController) AddNewProduct(ctx *gin.Context) {
+	productCreate := entity.ProductCreate{}
+	err := ctx.ShouldBind(&productCreate)
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data, err := ps.productService.CreateNewProduct(ctx, productCreate)
+
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := common.BuildResponse(true, "Product Created", data)
+	ctx.JSON(http.StatusCreated, response)
+}
+func (ps *productController) GetAllProducts(ctx *gin.Context) {
+	data, err := ps.productService.GetAllProducts(ctx.Request.Context())
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := common.BuildResponse(true, "OK", data)
+	ctx.JSON(http.StatusCreated, response)
+}
+
+func (ps *productController) UpdateProductByID(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("productId"), 10, 64)
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	productUpdate := entity.ProductUpdate{}
+	err = ctx.ShouldBind(&productUpdate)
+
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data, err := ps.productService.UpdateProductById(ctx.Request.Context(), productUpdate, uint64(id))
+
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := common.BuildResponse(true, "OK", data)
+	ctx.JSON(http.StatusOK, response)
+}
+
+func (ps *productController) DeleteProductById(ctx *gin.Context) {
+	id, err := strconv.ParseInt(ctx.Param("productId"), 10, 64)
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+	err = ps.productService.DeleteProductById(ctx.Request.Context(), uint64(id))
+	if err != nil {
+		response := common.BuildErrorResponse("Something went wrong", err.Error(), common.EmptyObj{})
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := common.BuildResponse(true, "Product Deleted", common.EmptyObj{})
+	ctx.JSON(http.StatusOK, response)
+}
